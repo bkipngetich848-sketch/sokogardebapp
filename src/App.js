fc1f6293f6a28c282f28476a2faa8df951c9431a
bkipngetich848-sketch <bkipngetich848@gmail.com>
2026-06-05 11:58:30 +0300

    sokogardenapp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import Getproductss from './components/Getproductss';
+import Signup from './components/Signup';
+import Signin from './components/Signin';
+import Addproduct from './components/Addproduct';
+import Notfound from './components/Notfound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+   <Router>
+     <div className="App">
+      <header className="App-header">        
+        <h1>Sokogarden Buy & Sell Online</h1>       
       </header>
+
+      <Routes>
+        <Route path='/' element={<Getproductss/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/addproduct' element={<Addproduct/>}/>
+        <Route path='/*'element={<Notfound/>}/>
+        
+
+      </Routes>
     </div>
+   </Router>
   );
 }
 
---
+++ src/components/Getproductss.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Getproductss = () => {
+  return (
+    <div>
+      <h1>Welcome To The Get Products</h1>
+    </div>
+  )
+}
+
+export default Getproductss
